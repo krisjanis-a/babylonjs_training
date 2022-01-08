d4222031c90cb1956ffa0ccc90fbdc305700e1aa
krisjanis-a <krishjanis96@gmail.com>
2022-01-08 20:28:00 +0200

    second

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engi
 const createScene = function () {
   const scene = new BABYLON.Scene(engine);
 
-  //   BABYLON.SceneLoader.ImportMeshAsync(
-  //     "",
-  //     "https://assets.babylonjs.com/meshes/",
-  //     "box.babylon"
-  //   );
+  // The first parameter can be set to null to load all meshes and skeletons
+  const card = BABYLON.SceneLoader.ImportMesh(
+    "",
+    "./",
+    "Card_QD.gltf",
+    scene,
+    function (meshes, particleSystems, skeletons) {
+      // do something with the meshes and skeletons
+      // particleSystems are always null for glTF assets
+    }
+  );
 
-  BABYLON.meshBuilder.CreateBox("box", {});
+  const box = BABYLON.MeshBuilder.CreateBox("box", {
+    height: 3,
+    width: 2,
+  });
+
+  box.position.y = 1.75;
+
+  const ground = BABYLON.MeshBuilder.CreateGround("ground", {
+    width: 10,
+    height: 10,
+  });
 
   const camera = new BABYLON.ArcRotateCamera(
     "camera",
@@ -20,7 +36,9 @@ const createScene = function () {
     15,
     new BABYLON.Vector3(0, 0, 0)
   );
+
   camera.attachControl(canvas, true);
+
   const light = new BABYLON.HemisphericLight(
     "light",
     new BABYLON.Vector3(1, 1, 0)
