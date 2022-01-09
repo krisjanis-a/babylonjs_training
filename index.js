b7397fae9278a51f0ece986a1d3b2b32682f52fa
krisjanis-a <krishjanis96@gmail.com>
2022-01-09 13:42:52 +0200

    build dwellings functions

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ const createScene = function () {
   // Scene creation
   const scene = new BABYLON.Scene(engine);
 
-  const house1 = buildHouse("CUBE", 3, 2, 2);
-  const house2 = buildHouse("SEMI_DETACHED", 5, 3, 4);
-  buildGround(50, 50);
-
-  house1.position.x = 10;
-  house2.position.x = 5;
-  house2.position.z = -7;
+  // Add objects to scene
+  const ground = buildGround(25, 25);
+  buildDwellings();
 
   // CAMERA
   const camera = new BABYLON.ArcRotateCamera(
@@ -32,6 +28,58 @@ const createScene = function () {
   );
 
   return scene;
+};
+
+const buildDwellings = () => {
+  const places = []; //each entry is an array [house type, rotation, x, z]
+  places.push([1, -Math.PI / 16, -6.8, 2.5]);
+  places.push([3, -Math.PI / 16, -4.5, 3]);
+  places.push([2, -Math.PI / 16, -1.5, 4]);
+  places.push([4, -Math.PI / 3, 1.5, 6]);
+  places.push([2, (15 * Math.PI) / 16, -6.4, -1.5]);
+  places.push([1, (15 * Math.PI) / 16, -4.1, -1]);
+  places.push([2, (15 * Math.PI) / 16, -2.1, -0.5]);
+  places.push([1, (5 * Math.PI) / 4, 0, -1]);
+  places.push([1, Math.PI + Math.PI / 2.5, 0.5, -3]);
+  places.push([2, Math.PI + Math.PI / 2.1, 0.75, -5]);
+  places.push([4, Math.PI + Math.PI / 2.25, 0.75, -8]);
+  places.push([3, Math.PI / 1.9, 4.75, -1]);
+  places.push([1, Math.PI / 1.95, 4.5, -3]);
+  places.push([2, Math.PI / 1.9, 4.75, -5]);
+  places.push([1, Math.PI / 1.9, 4.75, -7]);
+  places.push([3, -Math.PI / 3, 5.25, 2]);
+  places.push([1, -Math.PI / 3, 6, 4]);
+
+  //Create instances from the first two that were built
+  const houses = [];
+  for (let i = 0; i < places.length; i++) {
+    houses[i] = addHouseToScene(places[i][0]);
+
+    houses[i].rotation.y = places[i][1];
+    houses[i].position.x = places[i][2];
+    houses[i].position.z = places[i][3];
+  }
+};
+
+const addHouseToScene = (houseType) => {
+  switch (houseType) {
+    case 1: {
+      const cubeHouseSmall = buildHouse("CUBE", 1, 1, 1);
+      return cubeHouseSmall;
+    }
+    case 2: {
+      const cubeHouseBig = buildHouse("CUBE", 1.5, 1.5, 1.5);
+      return cubeHouseBig;
+    }
+    case 3: {
+      const semiDetachedHouseSmall = buildHouse("SEMI_DETACHED", 1.5, 1, 1);
+      return semiDetachedHouseSmall;
+    }
+    case 4: {
+      const semiDetachedHouseBig = buildHouse("SEMI_DETACHED", 2.5, 1.25, 1.5);
+      return semiDetachedHouseBig;
+    }
+  }
 };
 
 //! BUILD FUNCTIONS
@@ -91,14 +139,14 @@ const buildRoof = (width, height, depth) => {
     "https://assets.babylonjs.com/environments/roof.jpg"
   );
 
-  // roofMat.wireframe = true;
-
+  // Roof shape => cylinder with triangle base
   const roof = BABYLON.MeshBuilder.CreateCylinder("roof", {
     diameter: 1.5,
     height: 1,
     tessellation: 3,
   });
 
+  // Scale and rotate to correct position
   roof.scaling.x = height * 0.5;
   roof.scaling.y = width * 1.1;
   roof.scaling.z = depth;
@@ -135,7 +183,8 @@ const buildGround = (width, height) => {
   });
 
   const groundMat = new BABYLON.StandardMaterial("groundMat");
-  groundMat.diffuseColor = new BABYLON.Color3(0.25, 0.5, 0);
+
+  groundMat.diffuseColor = new BABYLON.Color3(0.15, 0.25, 0);
   ground.material = groundMat; //Place the material property of the ground
 
   return ground;
